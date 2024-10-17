3127d14f5577e8df45ddff34770080df798d5b54
nozomi-koborinai <kobofender@gmail.com>
2024-10-18 08:51:06 +0900

    feat: vertex ai genkit function

---
+++ functions/src/genkit-functions/generateImage.ts
@@ -0,0 +1,23 @@
+import { prompt } from '@genkit-ai/dotprompt'
+import * as genkitFunctions from '@genkit-ai/firebase/functions'
+import * as z from 'zod'
+import { googleAIapiKey } from '../config/firebase'
+import { generateImageInputSchema } from '../schemas/generateImageInputSchema'
+
+// 画像出力の Genkit Functions
+export const generateImage = genkitFunctions.onFlow(
+  {
+    name: `generateImage`,
+    httpsOptions: {
+      cors: true,
+      secrets: [googleAIapiKey],
+    },
+    inputSchema: generateImageInputSchema,
+    authPolicy: genkitFunctions.noAuth(),
+  },
+  async (input) => {
+    const generateImagePrompt = await prompt<z.infer<typeof generateImageInputSchema>>(`generateImage`)
+    const result = await generateImagePrompt.generate({ input })
+    return result.media()
+  }
+)
