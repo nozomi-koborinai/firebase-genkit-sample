7cba59a9ec829faf958ca806744b17c809be832f
nozomi-koborinai <kobofender@gmail.com>
2024-10-18 08:51:17 +0900

    feat: deploy to genkit run functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,5 @@
 export { analyzeStorageImage } from './genkit-functions/analyzeStorageImage'
+export { analyzeWebContents } from './genkit-functions/analyzeWebContents'
 export { anonymousFirestoreChatbot } from './genkit-functions/anonymousFirestoreChatbot'
+export { generateImage } from './genkit-functions/generateImage'
 export { noAuthFunction } from './genkit-functions/noAuthFunction'
