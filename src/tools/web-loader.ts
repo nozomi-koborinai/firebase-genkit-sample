782bd3acc91ab9b0ea320f2f5758b1a8e7263659
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2025-04-22 00:00:09 +0900

    refactor: web loader

---
+++ src/tools/web-loader.ts
@@ -0,0 +1,22 @@
+import { ai } from '../genkit';
+import { z } from 'zod';
+import * as cheerio from 'cheerio'
+
+export const webLoader = ai.defineTool(
+  {
+    name: `webLoader`,
+    description: `Accesses the specified URL and retrieves the content of the webpage.`,
+    inputSchema: z.object({ url: z.string().url() }),
+    outputSchema: z.string(),
+  },
+  async ({ url }) => {
+    const res = await fetch(url)
+    const html = await res.text()
+    const $ = cheerio.load(html)
+    $(`script, style, noscript`).remove()
+    if ($(`article`).length) {
+      return $(`article`).text()
+    }
+    return $(`body`).text()
+  }
+)
